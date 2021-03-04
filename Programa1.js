9ace69033040ba0854113d1f8d7a613ef4d3502d
Your Name <you@example.com>
2021-03-04 08:36:48 -0500

    Primer repositorio

---
+++ Programa1.js
@@ -0,0 +1,56 @@
+//Título: Programa #1 Javascript
+//Declaración de Variable 
+var numero = 365; 
+console.log(numero);
+numero=8;
+console.log(numero);
+
+//Declaración de Constante 
+const mensaje= "Bienvenido a Valencia";
+//mensaje = "Bienvenido a Quevedo";
+console.log(mensaje); 
+
+//Características de personas 
+const nombre="Juan";
+const apellidoPaterno="Andrade";
+const apellidoMaterno="Santacruz";
+var edad="18";
+const lugarDeNacimiento="Quevedo,Los Ríos,Ecuador";
+console.log(nombre);
+
+//Características de un Carro
+var matrícula="LR-205";
+const marca="RollsRoyce";
+var color="Negro";
+const númeroDePuerta="5";
+var precio="10000";
+console.log(color);
+
+//Tipos de Datos 
+var numero0=1;
+
+var texto1= "carro";
+
+var datoVerdadero=true;
+var datoFalso=false;
+
+var condición = null; 
+console.log(condición);
+//Undefined
+var datoindefinido;
+console.log(datoindefinido);
+
+//Dato Arreglo
+const arregloNumeros = [8, 9, 10];
+const arregloNombres = ["Juan","Carlos","Patricio"];
+const arregloMixto=[8,"Juan",64,"Patricio"];
+
+//Dato de Objeto
+var persona={
+    nombre1:"Diego",    
+    appellido1: "Andrade",
+    apellido2:"Santacruz",
+    edad1: 18,
+    lugardeNacimiento1: "Quevedo"
+}
+console.log(persona);
